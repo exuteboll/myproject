765fe13b6a535bedef08ff86a3f13f7e06378b2c
DaniilDemidov <exuteboll4@gmail.com>
2025-12-03 11:41:36 +0200

    готовая с админ панелью

--- WebApplication3/obj/Release/net8.0/WebApplication3.AssemblyInfo.cs
+++ WebApplication3/obj/Release/net8.0/WebApplication3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f18b2ba3e5b2d226c9c15d893e765443685d9b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2174ebe67e8bb5fda636bc4123fbd3d54a71994")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
